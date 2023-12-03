061a003544b6a1bab2520a8adbf9b7c42779105b
ameer017 <rajiabdullahi907@gmail.com>
2023-12-03 18:48:56 +0100

    008

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import ChatPage from "./pages/ChatPage";
 import { SettingsPage } from "./pages/SettingsPage";
 function App() {
 	const user = useRecoilValue(userAtom);
+	// console.log(user)
 	const { pathname } = useLocation();
 	return (
 		<Box position={"relative"} w='full'>
