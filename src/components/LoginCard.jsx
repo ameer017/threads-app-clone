e4077e10badf7d1203366dd3f2aa57e381a2d150
ameer017 <rajiabdullahi907@gmail.com>
2023-12-14 12:52:32 +0100

    optimize

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -35,7 +35,7 @@ export default function LoginCard() {
 		setLoading(true);
 		try {
 			const res = await fetch("/api/users/login", {
-				method: "post",
+				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
 				},
